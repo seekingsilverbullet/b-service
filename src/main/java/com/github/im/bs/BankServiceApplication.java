20278b451dbcba5d3d589e56414681dae7aa6aaa
Ivan Mochalov <iv.moc@yandex.ru>
2020-03-15 13:38:37 +0300

    added spring boot starter

---
+++ src/main/java/com/github/im/bs/BankServiceApplication.java
@@ -0,0 +1,16 @@
+/*
+ * 3/15/20, 12:31 PM
+ * ivan
+ */
+
+package com.github.im.bs;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BankServiceApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(BankServiceApplication.class, args);
+    }
+}
